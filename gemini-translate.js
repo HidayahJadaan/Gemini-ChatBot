4a15f7fc01572bf51e3d59a29f955bf83cdb6395
HEDAIA KHALIL ALJADAAN <hkaljadaan18@cit.just.edu.jo>
2024-08-02 19:52:57 +0300

    Uploading with UI

--- gemini-translate.js
+++ gemini-translate.js
@@ -2,7 +2,7 @@ import dotenv from "dotenv";
 import { GoogleGenerativeAI } from "@google/generative-ai";
 import pkg from '@google-cloud/translate';
 const { Translate } = pkg.v2; 
-import readline from "readline";
+import readline from "readline"; // for reading input from the command line.
 dotenv.config();
 
 // Check if API_KEY or GOOGLE_TRANSLATE_API_KEY is loaded correctly
@@ -11,15 +11,20 @@ if (!process.env.API_KEY || !process.env.GOOGLE_TRANSLATE_API_KEY) {
   process.exit(1);
 }
 
-// Initialize Google Generative AI and Translate client
+// 1. Setting up the connection to Google Generative AI.
 const genAI = new GoogleGenerativeAI(process.env.API_KEY);
+// 2.  setting up the connection to Google Translate API.
 const translate = new Translate({ key: process.env.GOOGLE_TRANSLATE_API_KEY });
 
+
+// Sets up a readline interface to interact with the user via the command line.
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
 
+
+// Takes text and a target language code and translates the text using Google Translate.
 async function translateText(text, targetLanguage) {
   try {
     const [translation] = await translate.translate(text, targetLanguage);
@@ -32,8 +37,11 @@ async function translateText(text, targetLanguage) {
 
 async function run() {
   try {
+
+    // Initializes the Generative AI model (gemini-pro) and starts a chat session.
     const model = genAI.getGenerativeModel({ model: "gemini-pro" });
 
+    // Chat Interaction
     const chat = model.startChat({
       history: [],
       generationConfig: {
@@ -41,12 +49,16 @@ async function run() {
       },
     });
 
-    async function askAndResponse() {
+
+    async function askAndResponse() { 
+      // 1. Prompts the user for input.     
       rl.question("YOU: ", async (msg) => {
         if (msg.toLowerCase() === "exit") {
           rl.close();
         } else {
           try {
+
+            // 2. Sends the input message to the chatbot.
             const result = await chat.sendMessage(msg);
             const response = await result.response;
 
